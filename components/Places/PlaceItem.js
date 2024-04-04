879cfe22d507c6a7e6827053d227aac1f446ca3a
vianrove <vianrove@gmail.com>
2024-04-04 12:55:54 -0500

    Creación de componentes PlacesList y PlaceItem

---
+++ components/Places/PlaceItem.js
@@ -0,0 +1,19 @@
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+
+const PlaceItem = ({ place, onSelect }) => {
+  return (
+    <Pressable onPress={onSelect}>
+      <Image source={{ uri: place.imageUri }} />
+      <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default PlaceItem;
+
+const styles = StyleSheet.create({
+    
+});
