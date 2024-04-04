8efde1cbdf6c0484c47c4a5dd3fcbd90d363f1f2
vianrove <vianrove@gmail.com>
2024-04-04 12:57:11 -0500

    Creación de pantallas AllPlaces y AddPlace

---
+++ screens/AddPlace.js
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+import PlaceForm from "../components/Places/PlaceForm";
+
+const AddPlace = () => {
+    return (
+        <View>
+            <PlaceForm />
+        </View>
+    )
+}
+
+export default AddPlace;
---
+++ screens/AllPlaces.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import PlacesList from "../components/Places/PlacesList";
+
+const AllPlaces = () => {
+  return <View>
+    <PlacesList />
+  </View>;
+};
+
+export default AllPlaces;
