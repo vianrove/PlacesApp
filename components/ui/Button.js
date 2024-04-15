175e51afb9f5df7ed6bae78ed9e299da99296eba
vianrove <vianrove@gmail.com>
2024-04-15 09:58:28 -0500

    Creación de componente Button

---
+++ components/ui/Button.js
@@ -0,0 +1,38 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+import { Colors } from "../../constants/colors";
+
+const Button = ({ onPress, children }) => {
+  return (
+    <Pressable
+      style={({ pressed }) => [styles.button, pressed && styles.pressed]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{children}</Text>
+    </Pressable>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    margin: 4,
+    backgroundColor: Colors.primary800,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+    shadowOpacity: 0.15,
+    borderRadius: 2,
+  },
+  pressed: {
+    opacity: 0.55,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 16,
+    color: Colors.primary50,
+  },
+});
