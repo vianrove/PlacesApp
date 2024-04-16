b5d56381973841bfd29462452a4ea7a7f88b6c93
vianrove <vianrove@gmail.com>
2024-04-16 13:18:22 -0500

    Implementación de lógica para mostrar los Places

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
+import { StyleSheet, View } from "react-native";
+import Button from "../components/ui/Button";
 
-const AllPlaces = () => {
-  return <PlacesList />;
+const AllPlaces = ({ route }) => {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  // useEffect hook to handle updates when route params change
+  useEffect(() => {
+    if (route.params && route.params.place) {
+      // Update loadedPlaces only when a new place is passed via route params
+      setLoadedPlaces((prevPlaces) => [...prevPlaces, route.params.place]);
+    }
+  }, [route.params]);
+
+  const showPlaces = () => {
+    console.log(loadedPlaces);
+  };
+
+  return (
+    <View style={styles.container}>
+      <PlacesList places={loadedPlaces} />
+      <Button onPress={showPlaces}>Show Places</Button>
+    </View>
+  );
 };
 
 export default AllPlaces;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "space-between",
+  },
+});
